b51cfc552842b8d917ea437efa1799d9539ada62
Deyvid Ibarra <84426639+Deyvid12@users.noreply.github.com>
2021-06-03 19:02:09 -0500

    Update main.cpp

--- l-PARCIAL/Taller 6 Memoria Dinamica/Memoria Dinamica/main.cpp
+++ l-PARCIAL/Taller 6 Memoria Dinamica/Memoria Dinamica/main.cpp
@@ -1,3 +1,9 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores:  Godoy Johan, Ibarra Deyvid, Riascos Erick, Sandoval Leonardo
+Deber de Memoria Dinamica
+Fecha creación: 03/06/2021
+Fecha modificación: 03/05/2021*/
 #include <iostream>
 #include "Operaciones.h"
 
