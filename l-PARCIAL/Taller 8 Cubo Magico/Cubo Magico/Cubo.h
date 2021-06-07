22785f79662dd68b4fe3b8ea8d1088bf0050151f
Erick Riascos <84427196+ErickRiascos@users.noreply.github.com>
2021-06-06 21:21:52 -0500

    Update Cubo.h

--- l-PARCIAL/Taller 8 Cubo Magico/Cubo Magico/Cubo.h
+++ l-PARCIAL/Taller 8 Cubo Magico/Cubo Magico/Cubo.h
@@ -1,3 +1,9 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores:  Riascos Erick
+Memoria Dinamica: Potencia de Matrices
+Fecha creación: 05/06/2021
+Fecha modificación: 05/05/2021*/
 #pragma once
 #include "Vector.h"
 class Cubo{
