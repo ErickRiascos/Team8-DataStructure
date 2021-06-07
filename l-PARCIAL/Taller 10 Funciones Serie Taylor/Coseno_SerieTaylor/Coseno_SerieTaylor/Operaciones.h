b9d287443c1e1edc3da2274487e5c57f67b5f101
JohanGodoy1011 <84431930+JohanGodoy1011@users.noreply.github.com>
2021-06-07 02:27:30 -0500

    Funcion Coseno Serie Taylor

---
+++ l-PARCIAL/Taller 10 Funciones Serie Taylor/Coseno_SerieTaylor/Coseno_SerieTaylor/Operaciones.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Operaciones
+{
+	private:
+	public:
+		double pot(double, int);
+		long int fact(int);
+		double radian(double);
+		double modulo(double);
+		double coseno(double);
+};
+
+
