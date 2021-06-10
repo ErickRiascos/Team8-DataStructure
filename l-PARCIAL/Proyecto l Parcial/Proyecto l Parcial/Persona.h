2e7f0594db3349249ce85f8f848890bf8dd38f2f
Leonardo Sandoval <84425837+lssandoval@users.noreply.github.com>
2021-06-10 12:55:11 -0500

    Create Persona.h

--- l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Persona.h
+++ l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Persona.h
@@ -1,5 +1,15 @@
 #pragma once
 class Persona
 {
+  private:
+	Nodo* primero;
+	Nodo* actual;
+  public:
+	bool listaVacia();
+	Persona();
+	void mostrar();
+	void insertar_Cabeza(int);
+	void eliminar_Cabeza();
+
 };
 
