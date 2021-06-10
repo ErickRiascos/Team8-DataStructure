d32ca29974d6783d70d431c81d3324ed6a6e4ace
Leonardo Sandoval <84425837+lssandoval@users.noreply.github.com>
2021-06-10 12:56:23 -0500

    Update Persona.cpp

--- l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Persona.cpp
+++ l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Persona.cpp
@@ -1,1 +1,40 @@
 #include "Persona.h"
+#include <iostream>
+bool Persona::listaVacia() {
+    return (this->primero == NULL);
+}
+Persona::Persona() {
+    this->primero = NULL;
+    this->actual = NULL;
+}
+
+void Persona::mostrar() {
+    Nodo* tmp = this->primero;
+    while (tmp) {
+        std::cout << tmp->valor << "->";
+        tmp = tmp->siguiente;
+    }
+    std::cout << "NULL" << std::endl;
+}
+void Persona::insertar_Cabeza(int val) {
+    Nodo* nuevo = new Nodo(val);
+    if (listaVacia()) {
+        this->primero = nuevo;
+    }
+    else {
+        this->actual->siguiente = nuevo;
+    }
+    this->actual = nuevo;
+}
+void Persona::eliminar_Cabeza() {
+    Nodo* aux_borrar;
+    Nodo* anterior = NULL;
+    aux_borrar = primero;
+    if (aux_borrar == NULL) {
+        std::cout << "No se apunta a ningun elemento, vacio\n";
+    }
+    else if (anterior == NULL) {
+        primero = primero->siguiente;
+        delete aux_borrar;
+    }
+}
