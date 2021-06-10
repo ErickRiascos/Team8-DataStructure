3b9bdca818de1dad1f2e10b4e7937e430161db44
Leonardo Sandoval <84425837+lssandoval@users.noreply.github.com>
2021-06-10 18:00:26 -0500

    Update Persona.h

--- l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Persona.h
+++ l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Persona.h
@@ -2,15 +2,6 @@
 #include "Nodo.h"
 class Persona
 {
-  private:
-	Nodo* primero;
-	Nodo* actual;
-  public:
-	bool listaVacia();
-	Persona();
-	void mostrar();
-	void insertar_Cabeza(int);
-	void eliminar_Cabeza();
 
 };
 
