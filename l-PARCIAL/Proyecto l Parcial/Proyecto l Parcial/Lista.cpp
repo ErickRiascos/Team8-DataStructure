31a976bac53847fba95975d2fb1af6858e74905f
Leonardo Sandoval <84425837+lssandoval@users.noreply.github.com>
2021-06-12 16:02:32 -0500

    Update Lista.cpp

--- l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Lista.cpp
+++ l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Lista.cpp
@@ -1,1 +1,40 @@
 #include "Lista.h"
+#include <iostream>
+Lista::Lista() {
+    this->primero = NULL;
+    this->actual = NULL;
+}
+void Lista::mostrar() {
+    Nodo* tmp = this->primero;
+    while (tmp) {
+        std::cout<<""<< tmp->usuario.getEdad() << "->";
+        tmp = tmp->siguiente;
+    }
+    std::cout << "NULL" << std::endl;
+}
+bool Lista::listaVacia() {
+    return (this->primero == NULL);
+}
+void Lista::insertar_Persona(Persona _persona) {
+    Nodo* nuevo = new Nodo(_persona);
+    if (listaVacia()) {
+        this->primero = nuevo;
+    }
+    else {
+        this->actual->siguiente = nuevo;
+    }
+    this->actual = nuevo;
+}
+void Lista::eliminar_Persona() {
+    Nodo* aux_borrar;
+    Nodo* anterior = NULL;
+    aux_borrar = primero;
+    if (aux_borrar == NULL) {
+        std::cout << "Lista de personas, vacia\n";
+        std::cout << "Debe ingresar previamente a un usuario valido\n";
+    }
+    else if (anterior == NULL) {
+        primero = primero->siguiente;
+        delete aux_borrar;
+    }
+}
