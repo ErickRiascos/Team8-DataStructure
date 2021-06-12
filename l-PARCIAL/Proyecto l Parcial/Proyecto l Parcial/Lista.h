7029ca2bad4641f6410f3e4d3b56264388d9d81f
Leonardo Sandoval <84425837+lssandoval@users.noreply.github.com>
2021-06-12 16:02:55 -0500

    Update Lista.h

--- l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Lista.h
+++ l-PARCIAL/Proyecto l Parcial/Proyecto l Parcial/Lista.h
@@ -1,5 +1,15 @@
 #pragma once
+#include "Nodo.h"
 class Lista
 {
-};
+	private:
+		Nodo* primero;
+		Nodo* actual;
+	public:
+		Lista();
+		bool listaVacia();
+		void mostrar();
+		void insertar_Persona(Persona);
+		void eliminar_Persona();
+	};
 
