256a13b172d38e343bd51e93afbeec963230c426
JohanGodoy1011 <84431930+JohanGodoy1011@users.noreply.github.com>
2021-06-15 18:48:05 -0500

    debug

---
+++ l-PARCIAL/Taller 9 Funciones Serie Taylor/Coseno_SerieTaylor/CosUnitTest/CosUnittest/CosenoTest.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Coseno_SerieTaylor/Operaciones.h"
+#include "../Coseno_SerieTaylor/Operaciones.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CosenoTest
+{
+	TEST_CLASS(CosenoTest)
+	{
+	public:
+
+		TEST_METHOD(pot_Test)
+		{
+			Operaciones s;
+			double x = 5;
+			int y = 6;
+			double z = s.pot(x,y);
+			Assert::AreNotEqual(z, 0);
+		}
+		TEST_METHOD(fact_Test)
+		{
+			Operaciones s;
+			int x = 5;
+			int y = s.factorial(x);
+			Assert::AreNotEqual(y, 0);
+		}
+		TEST_METHOD(grado_to_radian_and_calcular_Seno_Test)
+		{
+			Operaciones  s;
+			int n = 5;
+			int v = s.factorial(n);
+			double r = 90;
+			int x = s.grados_to_radian(r);
+			Assert::AreEqual(x, 1);
+		}
+		TEST_METHOD(radian_Test)
+		{
+			Operaciones s;
+			double x = 5;
+			double y = s.radian(x);
+			Assert::AreNotEqual(y, 0);
+		}
+		TEST_METHOD(modulo_Test)
+		{
+			Operaciones s;
+			double x = 5;
+			double y = s.modulo(x);
+			Assert::AreNotEqual(y, 0);
+		}
+		TEST_METHOD(coseno_Test)
+		{
+			Operaciones s;
+		    double x = 5;
+			double y = s.coseno(x);
+			Assert::AreEqual(y, 1);
+		}
+	};
+}
