5212be3e76ef13790422772d13671b260abbfb1f
JohanGodoy1011 <84431930+JohanGodoy1011@users.noreply.github.com>
2021-06-16 00:32:54 -0500

    Update Listas Dobles Test.cpp

--- l-PARCIAL/Taller 11 Listas Dobles Enlazadas/Listas Dobles Test/Listas Dobles Test.cpp
+++ l-PARCIAL/Taller 11 Listas Dobles Enlazadas/Listas Dobles Test/Listas Dobles Test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Listas Dobles Test/ListaDobleEnlazada.h"
+#include "../Listas Dobles Test/ListaDobleEnlazada.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,7 +13,26 @@ namespace ListasDoblesTest
 		
 		TEST_METHOD(insertarCabezaTest)
 		{
-			
+			ListaDobleEnlazada s;
+			int x;
+			s.insertarCabeza(x);
+			Assert::AreNorEqual(0);
 		}
+		TEST_METHOD(insertarColaTest)
+		{
+			ListaDobleEnlazada s;
+			int x;
+			s.insertarCola(x);
+			Assert::AreNorEqual(0);
+		}
+		TEST_METHOD(recorrerTest)
+		{
+			ListaDobleEnlazada s;
+			int x;
+			int y;
+			s.insertarCabeza(x,y);
+			Assert::AreNorEqual(0);
+		}
+
 	};
 }
