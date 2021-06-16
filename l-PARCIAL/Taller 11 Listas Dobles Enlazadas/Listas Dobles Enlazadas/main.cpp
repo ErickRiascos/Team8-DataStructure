cff2a108b05f4d90cef21366835f72ce06ea3fc2
Deyvid Ibarra <84426639+Deyvid12@users.noreply.github.com>
2021-06-16 07:40:53 -0500

    Update main.cpp

--- l-PARCIAL/Taller 11 Listas Dobles Enlazadas/Listas Dobles Enlazadas/main.cpp
+++ l-PARCIAL/Taller 11 Listas Dobles Enlazadas/Listas Dobles Enlazadas/main.cpp
@@ -1,3 +1,9 @@
+/*Universidad de las Fuerzas Armadas "ESPE"
+Software
+Autores:  Godoy Johan, Ibarra Deyvid, Riascos Erick, Sandoval Leonardo
+Taller Listas Dobles Enlazadas
+Fecha creación: 14/06/2021
+Fecha modificación: 14/06/2021*/
 #include <iostream>
 #include "ListaDobleEnlazada.h"
 using namespace std;
