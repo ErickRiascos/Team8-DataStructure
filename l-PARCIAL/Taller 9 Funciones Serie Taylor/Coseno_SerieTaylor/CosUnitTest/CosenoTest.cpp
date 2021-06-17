becb4bb909fa43587c177389b59630a31859c2bc
JohanGodoy1011 <84431930+JohanGodoy1011@users.noreply.github.com>
2021-06-17 15:57:02 -0500

    debug

---
+++ l-PARCIAL/Taller 9 Funciones Serie Taylor/Coseno_SerieTaylor/CosUnitTest/CosenoTest.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CosenoTaylor/Operaciones.h"
+#include "../CosenoTaylor/Operaciones.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CosenoTest
+{
+	TEST_CLASS(CosenoTest)
+	{
+	public:
+
+		TEST_METHOD(pot_Test)
+		{
+			Operaciones s;
+			double x = 5;
+			int y = 6;
+			double z = s.pot(x,y);
+			Assert::AreNotEqual(z, 0);
+		}
+		TEST_METHOD(fact_Test)
+		{
+			Operaciones s;
+			int x = 5;
+			int y = s.factorial(x);
+			Assert::AreNotEqual(y, 0);
+		}
+
+		TEST_METHOD(radian_Test)
+		{
+			Operaciones s;
+			double x = 5;
+			double y = s.radian(x);
+			Assert::AreNotEqual(y, 0);
+		}
+		TEST_METHOD(modulo_Test)
+		{
+			Operaciones s;
+			double x = 5;
+			double y = s.modulo(x);
+			Assert::AreNotEqual(y, 0);
+		}
+		TEST_METHOD(coseno_Test)
+		{
+			Operaciones s;
+		    double x = 5;
+			double y = s.coseno(x);
+			Assert::AreEqual(y, 1);
+		}
+	};
+}
---
+++ l-PARCIAL/Taller 9 Funciones Serie Taylor/Coseno_SerieTaylor/CosenoTaylor/Operaciones.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Operaciones
+{
+private:
+public:
+	double pot(double, int);
+	long int fact(int);
+	double radian(double);
+	double modulo(double);
+	double coseno(double);
+};
+
