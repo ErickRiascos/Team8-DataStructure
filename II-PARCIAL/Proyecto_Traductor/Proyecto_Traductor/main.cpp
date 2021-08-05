0c23c58575ea8379bffe52073aec531916a58cfb
ErickRiascos <egriascos@espe.edu.ec>
2021-08-04 21:09:28 -0500

    v1.3

--- II-PARCIAL/Proyecto_Traductor/Proyecto_Traductor/main.cpp
+++ II-PARCIAL/Proyecto_Traductor/Proyecto_Traductor/main.cpp
@@ -12,23 +12,25 @@ Fecha modificacion: 08/02/2021*/
 #include "conio.h"
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
+#include <clocale>
+#include <windows.h>
 using namespace std;
 bool contiene(const char* a,const char* b) {
 	bool c = false;
 	int j = 0;
-	char* aux = (char*)calloc(100, sizeof(char));
+	char* aux = (char*)calloc(20, sizeof(char));
 	for (int i = 0; i <= strlen(a); i++) {
-		if (*(a + i) == ' ')
-			i++;
-		else {
+		if (*(a + i) != ' '){
 			if (*(a + i) == ';' || *(a + i) == '\0') {
 				j = 0;
 				if (!strcmp(aux, b))
 					c = true;
-				aux = (char*)calloc(100, sizeof(char));
+				aux = (char*)calloc(20, sizeof(char));
 			}
-			else
+			else {
 				*(aux + j++) = *(a + i);
+			}
 		}
 		
 	}
@@ -39,18 +41,23 @@ void palabra(RadixTree r,Nodo*&raiz,char* cjPalabras) {
 	char* aux = (char*)calloc(100, sizeof(char));
 	int j = 0;
 	for (int i = 0; i < strlen(cjPalabras); i++){
-		if (*(cjPalabras+i)==';'|| *(cjPalabras + i) == ' ') {
-			string s = aux;
-			r.insertar(s, raiz);
-			aux = (char*)calloc(100, sizeof(char));
-			j = 0;
+		if (*(cjPalabras + i) == ' ') {
+			i++;
 		}
-		else
-			*(aux + j++) = *(cjPalabras + i);
+		else {
+			if (*(cjPalabras + i) == ';') {
+				string s = aux;
+				r.insertar(s, raiz);
+				aux = (char*)calloc(100, sizeof(char));
+				j = 0;
+			}
+			else
+				*(aux + j++) = *(cjPalabras + i);
+		}
 	}	
 }
 
-char* expEsp(const char* a) {
+char* expSec(const char* a) {
 	bool centinela = false;
 	char* aux = (char*)calloc(200, sizeof(char));
 	int j = 0;
@@ -67,7 +74,7 @@ char* expEsp(const char* a) {
 	return aux;
 }
 
-char* expIng(const char* a) {
+char* expPrim(const char* a) {
 	char* aux = (char*)calloc(200, sizeof(char));
 	int i = 0, j = 0;
 	while (*(a + i) != '/') {
@@ -85,7 +92,7 @@ char* ingresarLet(const char* msj)
 	char c;
 	printf("%s", msj);
 	while ((c = getch()) != 13) {
-		if (c >= 97 && c <= 122) {
+		if (c >= 97 && c <= 122 || (c == 32)) {
 			printf("%c", c);
 			*(datos + i++) = c;
 		}
@@ -95,6 +102,9 @@ char* ingresarLet(const char* msj)
 
 int main()
 {
+	setlocale(LC_CTYPE, "Spanish"); // Cambiar locale - Suficiente para máquinas Linux
+	SetConsoleCP(1252); // Cambiar STDIN -  Para máquinas Windows
+	SetConsoleOutputCP(1252); // Cambiar STDOUT - Para máquinas Windows
 	char* word = NULL;
 	Nodo* raizIng = new Nodo();
 	Nodo* raizEsp = new Nodo();
@@ -105,7 +115,10 @@ int main()
 	const char* opciones[] = { "Ingles-Español","Español-Ingles","Salir" };
 	string s;
 	char* aux = NULL;	
+	bool z = false;
+	ifstream f;
 	do {
+		z = false;
 		Menu m(titulo, opciones, 3);
 		aux = (char*)calloc(100, sizeof(char));
 		word = (char*)calloc(20, sizeof(char));
@@ -116,46 +129,94 @@ int main()
 			x = 0;
 			n = 0;
 			strcpy(word, ingresarLet("Ingrese palabra en ingles: "));
-			ifstream f("dic1.txt");
+			f.open("dic1.txt");
 			if (f.is_open()) {
 				while (!f.eof()&&x!=1) {
 					getline(f, s);
-					if (contiene(expIng(s.c_str()), word)) {
-						strcpy(aux, expIng(s.c_str()));
+					if (contiene(expPrim(s.c_str()), word)) {
+						strcpy(aux, expPrim(s.c_str()));
 						palabra(o, raizIng, aux);
 						x++;
+						z = true;
 					}
 					n++;
 				}
 				f.close();
 			}
 			else cout << "Error de apertura del archivo." << endl;
-			x = 0;
-			aux = (char*)calloc(100, sizeof(char));
-			f.open("dic1.txt");
-			if (f.is_open()) {
-				while (!f.eof()) {
-					if (x == n) {
-						strcpy(aux, (expEsp(s.c_str())));
-						palabra(o, raizEsp, aux);
+			if (z) {
+				x = 0;
+				aux = (char*)calloc(100, sizeof(char));
+				f.open("dic1.txt");
+				if (f.is_open()) {
+					while (!f.eof()) {
+						if (x == n) {
+							strcpy(aux, (expSec(s.c_str())));
+							palabra(o, raizEsp, aux);
+						}
+						getline(f, s);
+						x++;
 					}
+					f.close();
+				}
+				else cout << "Error de apertura del archivo." << endl;
+				o.buscar(raizEsp, aux);
+				cout << endl << "Traduccion: " << aux << endl;
+			}
+			else {
+				cout<< "\nLa palabra no existe en el arbol Radix, ni en la base del diccionario o ingreso una palabra incorrecta intentelo de nuevo...";
+			}
+			cout << endl;
+			system("pause");
+			break;
+		}
+		case 2: {
+			x = 0;
+			n = 0;
+			strcpy(word, ingresarLet("Ingrese palabra en espaniol: "));
+			f.close();
+			f.open("dic2.txt");
+			if (f.is_open()) {
+				while (!f.eof() && x != 1) {
 					getline(f, s);
-					x++;
+					if (contiene(expPrim(s.c_str()), word)) {
+						strcpy(aux, expPrim(s.c_str()));
+						palabra(o, raizEsp, aux);
+						x++;
+						z = true;
+					}
+					n++;
 				}
 				f.close();
 			}
 			else cout << "Error de apertura del archivo." << endl;
-			o.buscar(raizEsp, aux);
-			cout << endl << "Traduccion: " << aux << endl;
+			if (z) {
+				x = 0;
+				aux = (char*)calloc(100, sizeof(char));
+				f.open("dic2.txt");
+				if (f.is_open()) {
+					while (!f.eof()) {
+						if (x == n) {
+							strcpy(aux, (expSec(s.c_str())));
+							palabra(o, raizIng, aux);
+						}
+						getline(f, s);
+						x++;
+					}
+					f.close();
+				}
+				else cout << "Error de apertura del archivo." << endl;
+				cout << endl << "Traduccion: " << aux << endl;
+			}
+			else {
+				cout << "\nLa palabra no existe en el arbol Radix, ni en la base del diccionario o ingreso una palabra incorrecta intentelo de nuevo...";
+			}
+			cout << endl;
 			system("pause");
 			break;
 		}
-		case 2:
-
-			system("pause");
-			break;
 		case 3:
-
+			exit(0);
 			system("pause");
 			break;
 		}
